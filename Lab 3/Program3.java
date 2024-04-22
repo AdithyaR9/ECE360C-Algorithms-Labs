f6eac30d79fff7e385812179f1d88724a1ccc4db
AdithyaR9 <rocket_saturn@hotmail.com>
2024-04-22 11:35:51 -0500

    more conscise, and readable method, also works

--- Lab 3/Program3.java
+++ Lab 3/Program3.java
@@ -1,4 +1,5 @@
 package ProvidedFiles;
+
 //Name: Adithya Ramanathan
 //EID: ar74353
 
@@ -9,10 +10,6 @@ public class Program3 {
     ImpactCalculator calculator;    // the impact calculator
     int[] treatment_plan;           // array to store the treatment plan
 
-
-    // my vars
-    int[][] opts;   // maximum impact values for subproblems
-    int[][] khart;  // corresponding hours of medicine for opt impact
 
     public Program3() {
         this.calculator = null;
@@ -47,111 +44,39 @@ public class Program3 {
 
         int totalTime = calculator.getTotalTime();
         int numMedicines = calculator.getNumMedicines();
+        int[][] dp = new int[numMedicines + 1][totalTime + 1];
 
-        // Put your code here
-
-
-        // create and reset tables for solutions
-        this.opts = new int[numMedicines][totalTime + 1];
-        this.khart = new int[numMedicines][totalTime + 1];
-        for (int i = 0; i < numMedicines; i++) {
+        // fill dp table looking at each medicing and each possible allocation
+        for (int i = 1; i <= numMedicines; i++) {
             for (int j = 0; j <= totalTime; j++) {
-                this.opts[i][j] = -1;
-                this.khart[i][j] = -1;
+                for (int t = 0; t <= j; t++) {
+                    int impCurr = calculator.calculateImpact(i - 1, t) + dp[i - 1][j - t];
+                    if (impCurr > dp[i][j]) {
+                        dp[i][j] = impCurr;
+                        if (j == totalTime) {
+                            treatment_plan[i - 1] = t;
+                        }
+                    }
+                }
             }
         }
 
-        // computes the maximum impact recursively by considering all medicines up to the last one and up to the full available time
-        int optimalSol = computeOptimalImpact(numMedicines - 1, totalTime);
-
-        for (int i = 0; i < totalTime; i++) {
-            computeOptimalImpact(numMedicines - 1, i);
-        }
-
-
-        int tim = totalTime;
-        int meds = numMedicines - 1;
-        for (int x = tim; x >= 0; x--) {
-            if (this.opts[meds][x] < optimalSol) {
-                tim = x + 1;
-                break;
-            }
-        }
-        for (int x = meds; x >= 0; x--) {
-            if (this.opts[x][tim] < optimalSol) {
-                meds = x + 1;
-                break;
-            }
-        }
-
-        recurse(meds, tim);
-
-        return optimalSol;
+        // backtrack to get optimal treament time
+        recurseBcktrck(dp, numMedicines, totalTime);
+        return dp[numMedicines][totalTime];
     }
 
-
-    // backtracks through the khart table to determine the exact number of hours
-    // each medicine should be administered based on the previously computed solutions
-    public void recurse(int meds, int tim) {
-        int loc = tim;
-        for (int a = meds; a >= 0; a--) {
-            int kt = this.khart[a][loc];
-            this.treatment_plan[a] = kt;
-            loc = loc - kt;
-        }
-    }
-
-    // gets max value from given arr
-    public int max(int[] arr) {
-        if (arr.length == 0) {
-            return -1;
-        }
-
-        int locMax = 0;
-        int valMax = arr[0];
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > valMax) {
-                valMax = arr[i];
-                locMax = i;
+    //  backtracks to set treatment plan using existing dp table
+    private void recurseBcktrck(int[][] dp, int numMedicines, int totalTime) {
+        for (int i = numMedicines, j = totalTime; i > 0; i--) {
+            for (int t = 0; t <= j; t++) {
+                if (dp[i][j] == calculator.calculateImpact(i - 1, t) + dp[i - 1][j - t]) {
+                    treatment_plan[i - 1] = t;
+                    j -= t;
+                    break;
+                }
             }
         }
-
-        return locMax;
-    }
-
-    // Recursively computes the maximum possible impact for each subproblem
-    // for using up to meds medicines within tim hours
-    // stores the maximum possible impact and the corresponding hours in opts and khart
-
-    public int computeOptimalImpact(int meds, int tim) {
-        if (this.opts[meds][tim] != -1) {
-            return this.opts[meds][tim];
-        }
-
-        if (meds == 0) {
-            this.opts[meds][tim] = calculator.calculateImpact(meds, tim);
-            this.khart[meds][tim] = tim;
-            return this.opts[meds][tim];
-        }
-
-        if (tim == 0) {
-            this.opts[meds][tim] = 0;
-            this.khart[meds][tim] = 0;
-            return 0;
-        }
-
-        int[] choces = new int[tim + 1];
-        for (int i = 0; i <= tim; i++) {
-            choces[i] = calculator.calculateImpact(meds, i) + computeOptimalImpact(meds - 1, tim - i);
-        }
-
-        int maxIndex = max(choces);
-        int opt = choces[maxIndex];
-        this.opts[meds][tim] = opt;
-        this.khart[meds][tim] = maxIndex;
-
-        return opt;
     }
 
 
